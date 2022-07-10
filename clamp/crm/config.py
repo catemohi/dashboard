2fa241eb6a5b891acfc3e8eddb0d644ac791ca97
catemohi <catemohi@gmail.com>
2022-07-10 19:47:51 +0300

    PEP-8 рефакторинг

--- clamp/crm/config.py
+++ clamp/crm/config.py
@@ -1,17 +1,17 @@
-from pathlib import Path
 from json import load
+from pathlib import Path
 from typing import Mapping, NamedTuple
 
 
 CONFIG_FILE_PATH = Path(__file__).with_name("config.json")
 with open(CONFIG_FILE_PATH) as file:
     CONFIG = load(file)
-    
-#TODO прокинуть переменную из окружения
+
+# TODO прокинуть переменную из окружения
 NAUMEN_LOGIN = ''
-#TODO прокинуть переменную из окружения
+# TODO прокинуть переменную из окружения
 NAUMEN_PASSWORD = ''
-#TODO прокинуть переменную из окружения
+# TODO прокинуть переменную из окружения
 NAUMEN_DOMAIN = ''
 CONFIG.update({'auth': {'login': NAUMEN_LOGIN,
                         'password': NAUMEN_PASSWORD,
@@ -19,9 +19,9 @@ CONFIG.update({'auth': {'login': NAUMEN_LOGIN,
 
 
 class CreateParams(NamedTuple):
-    
+
     """Класс данных для хранения данных для создания отчета в CRM Naumen.
-    
+
         Attributes:
             url: ссылка для создания отчета
             uuid: идентификатор обьекта
@@ -41,9 +41,9 @@ class CreateParams(NamedTuple):
 
 
 class FindParams(NamedTuple):
-    
+
     """Класс данных для хранения сформированного запроса к CRM Naumen.
-    
+
         Attributes:
             url: ссылка
             headers: headers для запроса
@@ -60,19 +60,21 @@ class FindParams(NamedTuple):
 
 def get_params_create_report(report_name: str) -> CreateParams:
     """Функция которая достает необходимые параметры из конфигурационного файла.
-    
+
     Args:
         report_name: название отчета
-        
+
     Returns:
         Коллекцию параметров.
-        
+
     Raises:
-    
+
     """
-    data_create = CreateParams('','',{},{},{},False,0,0)
-    reports_name = [key for key, val in CONFIG.items() if "create request" in val]
-    if not report_name in reports_name:
+    data_create = CreateParams('', '', {}, {}, {}, False, 0, 0)
+    reports_name = [
+        key for key, val in CONFIG.items() if "create request" in val
+        ]
+    if report_name not in reports_name:
         return data_create
     url = CONFIG['url']['create']
     uuid = CONFIG[report_name]['uuid']
@@ -83,29 +85,29 @@ def get_params_create_report(report_name: str) -> CreateParams:
     delay_attems = CONFIG[report_name]['delay attems']['value']
     num_attems = CONFIG[report_name]['num attems']['value']
     params["param2"] = {'name': 'uuid', 'value': uuid}
-    
-    return CreateParams(url,uuid,headers,params,data,
-                        verify,delay_attems,num_attems)
+
+    return CreateParams(url, uuid, headers, params, data,
+                        verify, delay_attems, num_attems)
 
 
 def get_params_find() -> FindParams:
     """Функция которая достает необходимые параметры из конфигурационного файла.
-    
+
     Args:
         report_name: название отчета
-        
+
     Returns:
         Коллекцию параметров.
-        
+
     Raises:
-    
+
     """
     url = CONFIG['url']['open']
     headers = CONFIG['headers']
     data = {}
     params = {}
     verify = CONFIG['verify']['value']
-    return FindParams(url,headers,params,data,verify)
+    return FindParams(url, headers, params, data, verify)
 
 
 if __name__ == "__main__":
