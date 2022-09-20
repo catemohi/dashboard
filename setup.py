20ed4325b7fd0fa8767c0085a666083b2bfbc2b2
Catemohi <catemohi@gmail.com>
2022-09-20 16:49:26 +0300

    ...

--- setup.py
+++ setup.py
@@ -14,12 +14,8 @@ setup(
     lecense='GNU General Public License v3.0',
     packages=find_packages(),
     install_requires=[
-        'beautifulsoup4>=4.11.1',
-        'certifi>=2022.9.14',
-        'charset-normalizer>=2.1.1',
-        'idna>=3.4',
-        'requests>=2.28.1',
-        'soupsieve>=2.3.2.post1',
+        'beautifulsoup4==4.11.1',
+        'requests==2.28.1',
     ],
     include_package_data=True,
 )
