e1da9d7274de37787c92b3c92de0893f1598e288
Catemohi <catemohi@gmail.com>
2022-10-04 23:46:04 +0300

    Исправление ошибок.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
+from setuptools import find_packages, setup
+from os.path import dirname, join
 
 
 setup(
@@ -8,7 +8,8 @@ setup(
     author="catemohi",
     author_email="catemohi@gmail.com",
     description="API CRM системы, основанное на парсинге DOM-дерева.",
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description=open(join(dirname(__file__), 'README.md'),
+                          encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/catemohi/naumen-api",
     lecense='GNU General Public License v3.0',
